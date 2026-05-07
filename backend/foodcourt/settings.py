38d25d944919ceadee45ee35eaf6265e00c813f0
Rex <waiyankyawsdk1999@gmail.com>
2026-05-07 06:32:59 +0630

    add : Dockerfiles, nginx, reqirements for Deployment

--- backend/foodcourt/settings.py
+++ backend/foodcourt/settings.py
@@ -116,3 +116,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
 ]
 CORS_ALLOW_CREDENTIALS = True
+
+# Static files collected to this dir by collectstatic (used in Docker)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
